9f988547ce14241b5c20850bb89452ff283a3d53
meganthadev <meganthadev@gmail.com>
2020-09-21 20:47:20 -0700

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do
-      resources :products do 
-        resources :journal_entries
-      end 
-    end 
-end
+     resources :products do
+      resources :journal_entries
+   end
+  end  
+ end 
+ 
+end 
