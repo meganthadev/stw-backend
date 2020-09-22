4f7c354be481a8d80834c042d24afa984a127f44
meganthadev <meganthadev@gmail.com>
2020-09-21 21:55:09 -0700

    Add CRUD actions 2 Prod Contr

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::ProductsController < ApplicationController
+
+
+        def index 
+            @products = Product.all 
+            render json: @products
+        end 
+
+        def create
+            @product = Product.new(product_params)
+            if @product.save 
+                render json: @product 
+            else  
+                render json: {error: 'Error, could not save Product'}
+            end 
+        end 
+
+        def show 
+            @product = Product.find(params [:id])
+            render json: @product
+        end 
+
+        def update 
+            @product = Product.find(params [:id])
+            render json: @product
+        end 
+
+        def delete 
+            @product = Product.find(params [:id])
+            @product.destroy
+        end 
+
+     private 
+
+        def product_params 
+            params.require(:product).permit(:name, :image_url, :rating, :description, :ingredients)
+        end  
+
+
 end
