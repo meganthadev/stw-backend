c5def9befbbe7141b6f5f6c972ddd3f05fa798d7
meganthadev <meganthadev@gmail.com>
2020-09-22 23:38:35 -0700

    Change :journal_entry to :entry

--- app/serializers/journal_entry_serializer.rb
+++ app/serializers/journal_entry_serializer.rb
@@ -1,3 +1,3 @@
 class JournalEntrySerializer < ActiveModel::Serializer
-  attributes :id, :journal_entry, :entry_date, :entry_type, :product_id
+  attributes :id, :entry, :entry_date, :entry_type, :product_id
 end
